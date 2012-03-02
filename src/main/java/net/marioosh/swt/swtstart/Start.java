357d64740cb1264750367c34f3c2f63d022640b2
marioosh <marioosh.net@gmail.com>
2012-03-02 15:13:49 +0100

    simple button + dialog message

--- src/main/java/net/marioosh/swt/swtstart/Start.java
+++ src/main/java/net/marioosh/swt/swtstart/Start.java
@@ -1,8 +1,13 @@
 package net.marioosh.swt.swtstart;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.ApplicationWindow;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -18,13 +23,30 @@ public class Start extends ApplicationWindow {
 	protected Control createContents(Composite parent) {
 
 		/**
-		 * shell
+		 * app title, icon, size
 		 */
 		getShell().setText("Sample App");
 		getShell().setImage(new Image (getShell().getDisplay(), ClassLoader.getSystemResourceAsStream("Apple-256.png")));
 		getShell().setMinimumSize(700, 340);
-		// getShell().setLayout(new GridLayout(1, true));
+		
+		/**
+		 * main window layout
+		 */
+		getShell().setLayout(new GridLayout(1, true));
 
+		/**
+		 * button example
+		 */
+		Button button = new Button(getShell(), SWT.PUSH);
+		button.setText("Button");
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				MessageDialog dialog = new MessageDialog(getShell(), "Title", null, "Message ..... .", MessageDialog.INFORMATION, new String[] { "Ok" }, 0);
+				dialog.open();							
+			}
+		});
+		
 		// parent.pack();
 		return parent;
 	}
